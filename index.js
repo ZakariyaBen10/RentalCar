64890ae79eb8ab11bb1962a99f8c953eb8cd82a4
zakariyaben10 <133095930+ZakariyaBen10@users.noreply.github.com>
2023-12-28 05:56:53 +0100

    Connection to mongoDB
    Conected mongoDB to project and made again basic code to see if it's connect successfully

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_KEY, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
 
 app.listen(port, () => {
